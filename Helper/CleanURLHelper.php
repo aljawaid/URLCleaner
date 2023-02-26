63efd2c1611a0bb2e50895a97cca9aa0c71a989e
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-26 01:07:29 +0000

    Add function `newCoreRoutes()` to helper

--- Helper/CleanURLHelper.php
+++ Helper/CleanURLHelper.php
@@ -6,4 +6,18 @@ use Kanboard\Core\Base;
 
 class CleanURLHelper extends Base
 {
+    public function newCoreRoutes()
+    {
+        $routes = array(
+            // Default key for each will start from 0
+            array(
+                "before_route" => "?controller=TaskMovePositionController&action=show&task_id=662&project_id=37",
+                "after_route" => "/project/37/task/662/move",
+            ),
+            array(
+                "before_route" => "?controller=ActivityController&action=user",
+                "after_route" => "/my-activity"
+            ),
+        );
+    }
 }
