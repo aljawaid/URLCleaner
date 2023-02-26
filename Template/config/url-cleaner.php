58fc72b453616ab9efb29c7d713adae096ff10e1
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-26 01:13:47 +0000

    Add draft table

--- Template/config/url-cleaner.php
+++ Template/config/url-cleaner.php
@@ -29,4 +29,46 @@
             </form>
         </fieldset>
     </fieldset>
+    <?php if ($this->task->configModel->get('clean-url-options', '') == 'enable'): ?>
+        <div id="ApplicationPanel" class="panel">
+            <h3 class=""><?= t('Application URLs') ?></h3>
+            <div class="table-responsive table-responsive-sm">
+                <table class="table table-striped table-sm">
+                  <caption>URLs shown below extend existing core routes in the application.</caption>
+                  <thead class="thead-dark">
+                    <tr>
+                      <th scope="col">before (route)</th>
+                      <th scope="col">after (url)</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    $routes = $this->helper->cleanURLHelper->newCoreRoutes();
+                    ?>
+                    <?php foreach ($routes as $route): ?>
+
+
+                    <tr>
+                      <td><?= $this->helper->text->e($route['before_route']) ?></td>
+                      <td><?= $this->helper->text->e($route['after_route']) ?></td>
+                    </tr>
+                    <tr>
+                      <td>Core</td>
+                      <td>Thornton</td>
+                    </tr>
+                    <tr>
+                      <td>?controller=Bigboard&action=index&plugin=Bigboard</td>
+                      <td>/bigboard</td>
+                    </tr>
+                  </tbody>
+
+                    <?php endforeach ?>
+                </table>
+            </div>
+        </div>
+        <div id="PluginPanel" class="panel">
+            <h3 class=""><?= t('Plugin URLs') ?></h3>
+            <p class="">The URLs listed below are curated from plugins in the <?= $this->url->link(t('Extensions Directory'), 'PluginController', 'directory') ?> </p>
+        </div>
+    <?php endif ?>
 </div>
