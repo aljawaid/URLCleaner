af31992b1eae4e8771f4a7a1a735a6d3a335739c
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-26 01:11:23 +0000

    Add draft page content

---
+++ Template/config/url-cleaner.php
@@ -0,0 +1,6 @@
+<div class="page-margin">
+    <div class="page-header">
+        <h2 class="">Clean URL Management</h2>
+    </div>
+    <p class="">URLs are sanitized based on routes and activities within the application. Clean URLs improve the usability and accessibility of a website further improving user workflow and productivity.</p>
+</div>
