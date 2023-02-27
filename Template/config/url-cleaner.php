139a4c0f5d1347a4aed83a3e94f0f80d9f62a613
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-27 01:12:21 +0000

    Update table

--- Template/config/url-cleaner.php
+++ Template/config/url-cleaner.php
@@ -1,22 +1,25 @@
 <div class="page-margin">
     <div class="page-header">
-        <h2 class="">Clean URL Management</h2>
+        <h2 class=""><?= t('URL Management') ?></h2>
     </div>
-    <p class="">URLs are sanitized based on routes and activities within the application. Clean URLs improve the usability and accessibility of a website further improving user workflow and productivity.</p>
+    <p class="section-intro"><?= t('URLs are sanitized based on routes and activities within the application. Clean URLs improve the usability and accessibility of a website encouraging consistency and productivity. Learn more about clean URLs on Wikipedia.') ?> <a href="https://en.wikipedia.org/wiki/Clean_URL" class="" target="_blank" rel="noopener noreferrer" title="<?= t('Opens in a new window') ?>"><?= t('What are clean URLs?') ?></a>
+    </p>
     <fieldset class="">
-        <legend>Options</legend>
-        <fieldset class="">
-            <legend>Configuration</legend>
+        <legend><?= t('Options') ?></legend>
+        <fieldset class="options-section">
+            <legend><?= t('Configuration') ?></legend>
             <div class="url-detection">
             <?php if (ENABLE_URL_REWRITE): ?>
                 <i class="fa fa-toggle-on i-fw fa-lg pp-green" aria-hidden="true"></i> <?= t('URL rewriting is configured') ?>
+
             <?php else: ?>
-                <i class="fa fa-toggle-off i-fw fa-lg pp-red" aria-hidden="true"></i> <?= t('URL rewriting is not configured') ?> <a href="https://docs.kanboard.org/v1/admin/url-rewriting/" class="" target="_blank" rel="noopener noreferrer" title="<?= t('Opens in a new window') ?>"><?= t('Learn more') ?></a>
+                <i class="fa fa-toggle-off i-fw fa-lg pp-red" aria-hidden="true"></i> <?= t('URL rewriting is not configured') ?>
+                <a href="https://docs.kanboard.org/v1/admin/url-rewriting/" class="" target="_blank" rel="noopener noreferrer" title="<?= t('Opens in a new window') ?>"><?= t('Learn more') ?></a>
             <?php endif ?>
             </div>
         </fieldset>
-        <fieldset class="">
-            <legend>Activation</legend>
+        <fieldset class="options-section">
+            <legend><?= t('Activation') ?></legend>
             <form class="url-form" method="post" action="<?= $this->url->href('URLCleanerController', 'save', array('redirect' => 'show', 'plugin' => 'URLCleaner')) ?>" autocomplete="off">
                 <?= $this->form->csrf() ?>
                 <div class="url-radio-options">
@@ -32,23 +35,21 @@
     <?php if ($this->task->configModel->get('clean-url-options', '') == 'enable'): ?>
         <div id="ApplicationPanel" class="panel">
             <h3 class=""><?= t('Application URLs') ?></h3>
-            <div class="table-responsive table-responsive-sm">
-                <table class="table table-striped table-sm">
-                    <caption>URLs shown below extend existing core routes in the application.</caption>
+            <div class="table-responsive table-responsive-sm route-table-wrapper">
+                <table class="table table-sm route-table">
+                    <caption><?= t('The URLs shown below extend existing core routes in the application') ?></caption>
                     <thead class="thead-dark">
                         <tr>
-                            <th scope="col">before (route)</th>
-                            <th scope="col">after (url)</th>
+                            <th class="route-table-column" scope="col"><?= t('Before [Route]') ?></th>
+                            <th class="route-table-column" scope="col"><?= t('After [Clean URL]]') ?></th>
                         </tr>
                     </thead>
                     <tbody>
-                    <?php
-                    $routes = $this->helper->cleanURLHelper->newCoreRoutes();
-                    ?>
+                    <?php $routes = $this->helper->cleanURLHelper->newCoreRoutes(); ?>
                     <?php foreach ($routes as $route): ?>
                         <tr>
-                            <td><?= $this->helper->text->e($route['before_route']) ?></td>
-                            <td><?= $this->helper->text->e($route['after_route']) ?></td>
+                            <td class="route-table-row route-before"><?= $this->helper->text->e($route['before_route']) ?></td>
+                            <td class="route-table-row route-after"><?= $this->helper->text->e($route['after_route']) ?></td>
                         </tr>
                     </tbody>
                     <?php endforeach ?>
