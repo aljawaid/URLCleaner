6ae20386700d2985b0cd1bb4c97fdbd060f54e8b
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-27 04:32:01 +0000

    Page fixes

--- Template/config/url-cleaner.php
+++ Template/config/url-cleaner.php
@@ -5,13 +5,12 @@
     <p class="section-intro"><?= t('URLs are sanitized based on routes and activities within the application. Clean URLs improve the usability and accessibility of a website encouraging consistency and productivity. Learn more about clean URLs on Wikipedia.') ?> <a href="https://en.wikipedia.org/wiki/Clean_URL" class="" target="_blank" rel="noopener noreferrer" title="<?= t('Opens in a new window') ?>"><?= t('What are clean URLs?') ?></a>
     </p>
     <fieldset class="options-block">
-        <legend class="font-weight-bold"><?= t('Options') ?></legend>
-        <fieldset class="options-section column-25">
+        <legend class=""><?= t('Options') ?></legend>
+        <fieldset class="options-section">
             <legend><?= t('Configuration') ?></legend>
             <div class="url-detection">
             <?php if (ENABLE_URL_REWRITE): ?>
                 <i class="fa fa-toggle-on i-fw fa-lg pp-green" aria-hidden="true"></i> <?= t('URL rewriting is configured') ?>
-
             <?php else: ?>
                 <i class="fa fa-toggle-off i-fw fa-lg pp-red" aria-hidden="true"></i> <?= t('URL rewriting is not configured') ?>
                 <a href="https://docs.kanboard.org/v1/admin/url-rewriting/" class="" target="_blank" rel="noopener noreferrer" title="<?= t('Opens in a new window') ?>"><?= t('Learn more') ?></a>
@@ -38,7 +37,7 @@
             <h3 class=""><?= t('Application URLs') ?> <span class="route-count"><?= count($routesCore) ?></span></h3>
             <div class="table-responsive table-responsive-sm route-table-wrapper">
                 <table class="table table-sm route-table">
-                    <caption><?= t('The URLs shown below extend existing core routes in the application') ?></caption>
+                    <caption><?= t('The list below extends existing routes in the application') ?></caption>
                     <thead class="thead-dark">
                         <tr>
                             <th class="route-table-column" scope="col"><?= t('Before [Route]') ?></th>
